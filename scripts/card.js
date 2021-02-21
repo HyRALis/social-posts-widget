07d510dc5ea312a5ddbfddbc857d18dacb570443
HyRALis <petar.trajanoski.pt@gmail.com>
2021-02-21 23:49:43 +0100

    Made the modal and all the events for it

--- scripts/card.js
+++ scripts/card.js
@@ -5,24 +5,26 @@ import {
 } from "./createElements.js";
 
 const createCardElement = (cardPropertiesObject, key) => {
-  append("#widget .cards-container",createElement("div", "widget-card", `card${key}`));
+  append("#widget .cards-container",createElement("div", "widget-card", `card-${key}`, {
+      onclick: "openModal(this)"
+  }));
   // CREATE CHILDREN OF CARD
   ;
-  append(`#card${key}`, createElement("div", "card-content"));
+  append(`#card-${key}`, createElement("div", "card-content"));
 
-  append(`#card${key}`, createElement("div", "like-count"));
+  append(`#card-${key}`, createElement("div", "like-count"));
 
 
   // CREATE CHILDREN FOR CARD-CONTENT
   
-  append(`#card${key} .card-content`, createElement("div", "card-header"));
+  append(`#card-${key} .card-content`, createElement("div", "card-header"));
 
-  append(`#card${key} .card-content`, createElement("img", "card-image", "", {
+  append(`#card-${key} .card-content`, createElement("img", "card-image", "", {
     src: cardPropertiesObject.image,
     alt: `Posted image by ${cardPropertiesObject.name}`, 
   }));
 
-  append(`#card${key} .card-content`, createElement(
+  append(`#card-${key} .card-content`, createElement(
     "p",
     "card-caption",
     "",
@@ -32,16 +34,16 @@ const createCardElement = (cardPropertiesObject, key) => {
 
   // CREATE CHILDREN FOR CARD-HEADER
   
-  append(`#card${key} .card-header`, createElement("div", "poster-info-section"));
-  append(`#card${key} .card-header`, createElement("a", `network-svg ${cardPropertiesObject.source_type}`,"",{href: cardPropertiesObject.source_link}));
-  append(`#card${key} .poster-info-section`,createElement("img", "profile-image", "", {
+  append(`#card-${key} .card-header`, createElement("div", "poster-info-section"));
+  append(`#card-${key} .card-header`, createElement("a", `network-svg ${cardPropertiesObject.source_type}`,"",{href: cardPropertiesObject.source_link}));
+  append(`#card-${key} .poster-info-section`,createElement("img", "profile-image", "", {
     src: cardPropertiesObject.image,
     alt: `Avatar of ${cardPropertiesObject.name}`,
   }));
   
-  append(`#card${key} .poster-info-section`,createElement("div","post-info"));
+  append(`#card-${key} .poster-info-section`,createElement("div","post-info"));
   
-  append(`#card${key} .post-info`,createElement(
+  append(`#card-${key} .post-info`,createElement(
     "p",
     "poster-name",
     "",
@@ -50,7 +52,7 @@ const createCardElement = (cardPropertiesObject, key) => {
   ));
 
   
-  append(`#card${key} .post-info`,createElement(
+  append(`#card-${key} .post-info`,createElement(
     "p",
     "card-caption",
     "",
@@ -60,8 +62,8 @@ const createCardElement = (cardPropertiesObject, key) => {
 
   // CREATE THE LIKES COUNTER AREA
 
-  append(`#card${key} .like-count`, createElement("div","like-svg"));
-  append(`#card${key} .like-count`, createElement("span","","",{},cardPropertiesObject.likes));
+  append(`#card-${key} .like-count`, createElement("div","like-svg"));
+  append(`#card-${key} .like-count`, createElement("span","","",{},cardPropertiesObject.likes));
 };
 
 export default createCardElement;
--- scripts/widget.js
+++ scripts/widget.js
@@ -20,32 +20,112 @@ const createWidget = async () => {
   data = dataChunks(data, 4);
 
   append("#widget", createElement("div", "widget-container"));
-  
+
   append("#widget .widget-container", createElement("div", "cards-container"));
-    loadCards(data[numberOfPagesLoaded]);
-  append("#widget .widget-container", createElement("div", "loadmore-container"));
-  append("#widget .loadmore-container", createElement("button", "btn-primary load-more","",{onclick: 'loadMore()'},"LOAD MORE"));
+  loadCards(data[numberOfPagesLoaded]);
+  append(
+    "#widget .widget-container",
+    createElement("div", "loadmore-container")
+  );
+  append(
+    "#widget .loadmore-container",
+    createElement(
+      "button",
+      "btn-primary load-more",
+      "",
+      { onclick: "loadMore()" },
+      "LOAD MORE"
+    )
+  );
+
+  // CREATE MODAL
+  
 };
 
 const dataChunks = (dataArray, chunkLength) => {
-    let [...newData] = dataArray;
-    let chunkedData = [];
-    while (newData.length) {
-        chunkedData.push(newData.splice(0,chunkLength));
-    }
-    return chunkedData;
-}
+  let [...newData] = dataArray;
+  let chunkedData = [];
+  while (newData.length) {
+    chunkedData.push(newData.splice(0, chunkLength));
+  }
+  return chunkedData;
+};
 const loadCards = (data) => {
-    data.forEach((element,index) => {
-        createCardElement(element,numberOfPagesLoaded*4 + index);
-    });
-}
-window.loadMore = ( ) => {
-    numberOfPagesLoaded ++;
-    loadCards(data[numberOfPagesLoaded]);
-    if(numberOfPagesLoaded === data.length-1) {
-        document.getElementsByClassName("loadmore-container")[0].remove();
-    }        
+  data.forEach((element, index) => {
+    createCardElement(element, numberOfPagesLoaded * 4 + index);
+  });
+};
+
+window.loadMore = () => {
+  numberOfPagesLoaded++;
+  loadCards(data[numberOfPagesLoaded]);
+  if (numberOfPagesLoaded === data.length - 1) {
+    document.getElementsByClassName("loadmore-container")[0].remove();
+  }
+};
+
+window.openModal = ( el ) => {
+  const dataIndex =  el.id.split("-").pop();
+  const chunkIndex = parseInt(dataIndex / 4);
+  const itemIndex = dataIndex % 4;
+  
+  const cardData = data[chunkIndex][itemIndex]
+
+  append("body", createElement("div","modal-backdrop","",{onclick: "closeModal()"}));
+  window.addEventListener("keyup", (e)=> {
+    if (e.key === "Escape") {
+        closeModal();
+    }
+  }, {once: true})
+  append("#widget .modal-backdrop", createElement("div", "modal"));
+  append("#widget .modal", createElement("img", "modal-image","", {
+    src: cardData.image,
+    alt: `Posted image by ${cardData.name}`, 
+  }));
+  append("#widget .modal", createElement("div", "modal-content"));
+  append("#widget .modal-content", createElement("div", "modal-creator"));
+  append(`#widget .modal-creator`, createElement("div", "poster-info-section"));
+  append(`#widget .modal-creator`, createElement("a", `network-svg ${cardData.source_type}`,"",{href: cardData.source_link}));
+  append(`#widget .modal-content .poster-info-section`,createElement("img", "profile-image", "", {
+    src: cardData.profile_image,
+    alt: `Avatar of ${cardData.name}`,
+  }));
+  
+  append(`#widget .modal .poster-info-section`,createElement("div","post-info"));
+  
+  append(`#widget .modal .post-info`,createElement(
+    "p",
+    "poster-name",
+    "",
+    "",
+    cardData.name
+  ));
+
+  
+  append(`#widget .modal .post-info`,createElement(
+    "p",
+    "card-caption",
+    "",
+    "",
+    cardData.date
+  ));
+
+  append(`#widget .modal-content`, createElement(
+    "p",
+    "card-caption",
+    "",
+    "",
+    makeParagraphWithHashtagsLinks(cardData.caption)
+  ));
+
+  append("#widget .modal-content", createElement("div", "modal-likes"));
+
+  append(`#widget .modal-likes`, createElement("div","like-svg"));
+  append(`#widget .modal-likes`, createElement("span","","",{},cardData.likes));
+
 }
 
+window.closeModal = () => {
+    document.querySelector(".modal-backdrop").remove();
+}
 createWidget();
